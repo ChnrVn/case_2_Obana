b384f185ca71b7eddb7c81040adb2f604ef820e7
Zheyu <81740016+pnevmv@users.noreply.github.com>
2023-04-16 08:41:00 +0300

    Add files via upload

---
+++ src/main/java/com/example/obana1/Obana1Application.java
@@ -0,0 +1,14 @@
+package com.example.obana1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Obana1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Obana1Application.class, args);
+    }
+    
+}
